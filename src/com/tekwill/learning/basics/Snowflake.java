590f2e3ddd219d59296c9bd829a8e6d295d8337d
ElinaChi <stefan.elina@gmail.com>
2020-12-02 16:39:11 +0100

    solved exercise Snowflake

---
+++ src/com/tekwill/learning/basics/Snowflake.java
@@ -0,0 +1,16 @@
+package com.tekwill.learning.basics;
+
+public class Snowflake {
+    public static void main (String []args){
+
+                 System.out.println("\b  \\/");
+                 System.out.println("\b \\\\//");
+                 System.out.println("\\\\\\/// ");
+                 System.out.println("///\\\\\\ ");
+                  System.out.println(" //\\\\ \b");
+                   System.out.println("\b  /\\");
+
+
+    }
+}
+
