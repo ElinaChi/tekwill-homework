b995a009131f1a304f8254204d998ba3b04d163d
ElinaChi <stefan.elina@gmail.com>
2020-12-09 16:03:23 +0100

    solved exercise MyName

---
+++ src/com/tekwill/learning/basics/MyName.java
@@ -0,0 +1,12 @@
+package com.tekwill.learning.basics;
+
+public class MyName {
+    public static void main (String [] args){
+        System.out.println("MMMMM   I       S");
+        System.out.println("M       I       S");
+        System.out.println("MMMMM   I       S");
+        System.out.println("M       I       S");
+        System.out.println("MMMMM   IIIII   S");
+
+    }
+}
