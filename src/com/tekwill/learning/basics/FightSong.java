7e8d9609ad447ac69fc301fd87c1fc2eea2cd8db
ElinaChi <stefan.elina@gmail.com>
2020-12-09 16:22:21 +0100

    solved exercise FightSong

---
+++ src/com/tekwill/learning/basics/FightSong.java
@@ -0,0 +1,25 @@
+package com.tekwill.learning.basics;
+
+public class FightSong {
+    public static void main (String [] args){
+        System.out.print("Go,team, go!\n" +
+                        "You can do it.");
+
+        System.out.print("Go,team, go!\n" +
+                       "You can do it.\n" +
+                       "You're the best \n"+
+                       "In the West \n"+
+                       "Go,team, go!\n" +
+                       "You can do it.\n");
+
+        System.out.print("Go,team, go!\n" +
+                       "You can do it.\n" +
+                       "You're the best \n"+
+                       "In the West \n"+
+                       "Go,team, go!\n" +
+                       "You can do it.\n");
+
+        System.out.print("Go,team, go!\n" +
+                         "You can do it.");
+}
+}
