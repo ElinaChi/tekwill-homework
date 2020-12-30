9d1fee0d82dec14163abb47116b7345ba330059d
ElinaChi <stefan.elina@gmail.com>
2020-12-30 07:21:28 +0100

    add exercise with ASCII Code Converter

---
+++ src/com/tekwill/learning/flowcontrol/ifconstruction/ConverterToAsciiCode.java
@@ -0,0 +1,14 @@
+package com.tekwill.learning.flowcontrol.ifconstruction;
+
+
+import java.util.Scanner;
+
+public class ConverterToAsciiCode {
+    public static void main (String[] args){
+        System.out.print("Enter an ASCII code: \n");
+        Scanner scanner = new Scanner (System.in);
+        int AsciiCharacter= scanner.nextInt();
+        System.out.println((char)AsciiCharacter);
+
+    }
+    }
