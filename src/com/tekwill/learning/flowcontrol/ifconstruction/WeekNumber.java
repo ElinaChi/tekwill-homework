173848e96d1d5ea0d73e0f6e21c1c1599e9379cd
ElinaChi <stefan.elina@gmail.com>
2021-01-01 17:03:30 +0100

    add exercise Day of Week

---
+++ src/com/tekwill/learning/flowcontrol/ifconstruction/WeekNumber.java
@@ -0,0 +1,31 @@
+package com.tekwill.learning.flowcontrol.ifconstruction;
+
+import java.util.Scanner;
+
+public class WeekNumber {
+    public static void main(String args[]) {
+       Scanner scanner = new Scanner(System.in);
+        int weekNumber = scanner.nextInt();
+
+        System.out.printf("Input week number (1-7): %d\n",weekNumber);
+
+        switch (weekNumber){
+            case 1 :System.out.println( "Monday");
+                break;
+            case 2 :System.out.println( "Tuesday");
+                break;
+            case 3 :System.out.println( "Wednesday");
+                break;
+            case 4 :System.out.println( "Thursday");
+                break;
+            case 5 :System.out.println( "Friday");
+                break;
+            case 6 :System.out.println( "Saturday");
+                break;
+            case 7 :System.out.println( "Sunday");
+                break;
+        }
+
+    }
+
+}
