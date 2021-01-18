8784c8cc2885078e326c3b717c9046efab52eabc
ElinaChi <stefan.elina@gmail.com>
2021-01-18 15:53:23 +0100

    add exercise that calculate numbers divisible by 5 or 6

--- src/com/tekwill/learning/flowcontrol/loop/NumbersDivisible5Or6.java
+++ src/com/tekwill/learning/flowcontrol/loop/NumbersDivisible5Or6.java
@@ -1,2 +1,18 @@
-package com.tekwill.learning.flowcontrol.loop;public class NumbersDivisible5Or {
-}
+package com.tekwill.learning.flowcontrol.loop;
+
+public class NumbersDivisible5Or6 {
+    public static void main (String []args){
+        int x; int lineNumber = 0;
+
+        for (x = 100; x <= 200; x++)
+            if (x % 5 == 0 || x % 6 == 0) {
+                System.out.print(x + " ");
+                lineNumber++;
+                if (lineNumber % 10 == 0) System.out.println();
+
+            }
+
+        }
+    }
+
+
